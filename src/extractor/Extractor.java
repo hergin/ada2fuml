68d8bd500d968cdeefbc82543f5a88947a49856e
Huseyin Ergin <mersinus@gmail.com>
2019-07-03 11:29:26 -0400

    Extractor skeleton

---
+++ src/extractor/Extractor.java
@@ -0,0 +1,12 @@
+package extractor;
+
+import adaschema.CompilationUnit;
+import model.UML;
+
+public class Extractor {
+
+    public static UML extractHighLevelConcepts(CompilationUnit compilationUnit) {
+        return null;
+    }
+
+}
