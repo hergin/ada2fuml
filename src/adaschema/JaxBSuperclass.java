41922a644ab09ffb90411b42dd395f175e36319e
Huseyin Ergin <mersinus@gmail.com>
2019-07-08 11:53:31 -0400

    New schema with base class for parents and some helper functions. Test is updated w.r.t. them.

---
+++ src/adaschema/JaxBSuperclass.java
@@ -0,0 +1,14 @@
+package adaschema;
+
+public class JaxBSuperclass {
+
+    JaxBSuperclass parent;
+
+    public JaxBSuperclass getParent() {
+        return parent;
+    }
+
+    public void setParent(JaxBSuperclass parent) {
+        this.parent = parent;
+    }
+}
