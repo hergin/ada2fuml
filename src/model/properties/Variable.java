bd00b35ff61657915345cb453b59f52efcd6f490
Ergin, Huseyin <hergin@bsu.edu>
2020-08-16 14:32:20 -0400

    More ID pull up.

--- src/model/properties/Variable.java
+++ src/model/properties/Variable.java
@@ -7,22 +7,16 @@ import model.enums.VisibilityEnum;
 
 public class Variable extends PrimitiveProperty {
 
-    private String id;
     private boolean isConstantValue;
 
     public Variable (String name, VisibilityEnum visibility, TypeEnum type, Object defaultValue, boolean isConstant) {
         super(name, visibility, type, defaultValue);
-        id = Processor.uuidGenerator();
         this.isConstantValue = isConstant;
     }
 
 
     // The following is inherited from Property
     // public VisibilityEnum getVisibility()
-
-    public String getId() {
-        return id;
-    }
 
     public boolean isConstant() {
         return isConstantValue;
